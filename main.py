cf662bcb06f17e0d6c2cac4c6f3cddcdc3e18f73
caseyokane <casey.okane@uky.edu>
2021-08-08 21:07:14 -0400

    Add path/query parameters and include request body

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
-def index():
-    return {'data': {'name': 'Ca$ey'} }
+def index(limit=10, published:Optional[bool]=False, sort:Optional[str]=None):
+    if published:
+        return {'data': f'{limit} blogs from list' }
+    else:
+        return {'data': '0 blogs from list' }
 
 @app.get('/about')
 def about():
     return {'data': 'about page'}
+
+# Put static route before dynamic 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs' }
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id }
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return limit
+
+class Blog(BaseModel):
+    title: str
+    body: str 
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created as {blog.title}"}
